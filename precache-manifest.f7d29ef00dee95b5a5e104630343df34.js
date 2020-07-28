df4b8de1828f51e77a29271ce6cda535b916d5d0
jonathanalmoradie <almoradieCode@gmail.com>
2020-07-28 15:16:04 -0700

    Updates

---
+++ precache-manifest.f7d29ef00dee95b5a5e104630343df34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515fa5ef513afd1c8619c959c1d1b137",
+    "url": "/almoradieCode/index.html"
+  },
+  {
+    "revision": "2eeb2cfe2a1fc334cd5d",
+    "url": "/almoradieCode/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/almoradieCode/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/almoradieCode/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eeb2cfe2a1fc334cd5d",
+    "url": "/almoradieCode/static/js/main.8e480b97.chunk.js"
+  },
+  {
+    "revision": "53d36bb67ca0ea204f3f",
+    "url": "/almoradieCode/static/js/runtime-main.137f0a68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/almoradieCode/static/media/logo.5d5d9eef.svg"
+  }
+]);
